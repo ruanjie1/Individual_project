e71d6d18bf1489faea4fcb07b618df764733e734
ruanjie1 <1447427947@qq.com>
2019-09-26 15:35:55 +0800

    完善第一步功能三，对前面代码不灵活的地方进行修改，改善-d/-d -s命令混用出错

--- step_one.cpp
+++ step_one.cpp
@@ -6,16 +6,17 @@
 #include <vector>
 #include <queue>
 #include <io.h>
+#include <stdlib.h>
 #define ll long long
 using namespace std;
 map<string, int> cotword;// 统计单词出现的次数
 vector<string> word;// 统计单词是否出现
 vector<string> filenames; // 记录目录下文件名
 
-struct cmp{
+struct cmp{// 优先队列重载 < ，首选出现的次数排序，次选字典序
 	string words;
 	int cot;
-	friend operator <(cmp a, cmp b){// 优先队列重载 < ，首选出现的次数排序，次选字典序
+	friend operator <(cmp a, cmp b){
 		if(a.cot == b.cot) return a.words.compare(b.words) > 0;
 		return a.cot < b.cot;
 	}
@@ -28,19 +29,19 @@ path: 指定目录
 files: 保存结果
 fileType: 指定的文件格式，如 .jpg
 */
-//void getAllFiles(string path, vector<string>& files){
-//    long hFile = 0;// 文件句柄
-//    struct _finddata_t fileinfo;// 文件信息
-//    string p;
-//    if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1) {
-//        do {
-//            files.push_back(p.assign(path).append("\\").append(fileinfo.name));// 保存文件的全路径
-//           } while (_findnext(hFile, &fileinfo) == 0);  //寻找下一个，成功返回0，否则-1
-//        _findclose(hFile);
-//    }
-//}
+void getAllFiles(string path, vector<string>& files){ // 遍历文件夹中文件名
+    long hFile = 0;// 文件句柄
+    struct _finddata_t fileinfo;// 文件信息
+    string p;
+    if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1) {
+        do {
+            files.push_back(p.assign(path).append("\\").append(fileinfo.name));// 保存文件的全路径
+           } while (_findnext(hFile, &fileinfo) == 0);  //寻找下一个，成功返回0，否则-1
+        _findclose(hFile);
+    }
+}
 
-void getAllFiles(string path, vector<string>& files){ // 递归遍历子文件夹 获取文件名
+void getAllFiles_childCatalogue(string path, vector<string>& files){ // 递归遍历子文件夹 获取文件名
     long hFile = 0;//文件句柄
     struct _finddata_t fileinfo;//文件信息
     string p;
@@ -49,7 +50,7 @@ void getAllFiles(string path, vector<string>& files){ // 递归遍历子文件夹 获取文
             if ((fileinfo.attrib & _A_SUBDIR)) {  //比较文件类型是否是文件夹
                 if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0) {
                     files.push_back(p.assign(path).append("\\").append(fileinfo.name));
-                    getAllFiles(p.assign(path).append("\\").append(fileinfo.name), files);//递归搜索
+                    getAllFiles_childCatalogue(p.assign(path).append("\\").append(fileinfo.name), files);//递归搜索
                 }
             }
             else {
@@ -100,30 +101,26 @@ void wordF(){ // 将单词加入优先队列中，自动排序
 	}
 }
 
-void print_F(){ // 输出单词
-	cout << "不重复单词统计：" << endl;
-	cout << "出现次数" << '\t' << "单词"<< endl;
-	while(!que.empty()){
+void print_F(int n){ // 输出单词
+	cout << "序号" << "\t" << "出现次数" << '\t' << "单词" << endl;
+	int i = 0;
+	while(!que.empty() && i < n){
 		auto p = que.top(); que.pop();
-		cout << p.cot << '\t'<< p.words << endl;
+		cout << ++i << "\t" << p.cot << '\t'<< p.words << endl;
 //		cout << p.words << " ";
-	}cout << endl;
+	}
 }
 
-void solve_f(char *args[]){
+void solve_f(char *args[], int op){ // 对-f命令行参数进行处理
 	string s;
 	ifstream inf;
-	inf.open(args[2]);
-	freopen("step_one_out_NoRepetition.txt", "w", stdout); // 结果打印到文件——第1步out.txt 中
+	inf.open(args[op]);
 	while(getline(inf, s)) cotChar(s); // 单行读入进行操作
-	wordF();
-	print_F();
 	inf.close();
 }
 
-void solve_d(char *args[]){
-	getAllFiles(args[2], filenames);
-	freopen("step_one_out_NoRepetition.txt", "w", stdout); // 结果打印到文件——第1步out.txt 中
+void solve_d(char *args[], int op){ // 对-d命令行参数进行处理
+	getAllFiles(args[op], filenames);
 	for(int i = 0; i < filenames.size(); i++){
 		string s;
 		ifstream inf;
@@ -131,13 +128,10 @@ void solve_d(char *args[]){
 		while(getline(inf, s)) cotChar(s); // 单行读入进行操作
 		inf.close();
 	}
-	wordF();
-	print_F();
 }
 
-void solve_d_s(char *args[]){
-	getAllFiles(args[3], filenames);
-	freopen("step_one_out_NoRepetition.txt", "w", stdout); // 结果打印到文件——第1步out.txt 中
+void solve_d_s(char *args[], int op){ // 对-d -s 命令行参数进行处理
+	getAllFiles_childCatalogue(args[op], filenames);
 	for(int i = 0; i < filenames.size(); i++){
 		string s;
 		ifstream inf;
@@ -145,14 +139,33 @@ void solve_d_s(char *args[]){
 		while(getline(inf, s)) cotChar(s); // 单行读入进行操作
 		inf.close();
 	}
-	wordF();
-	print_F();
+}
+
+int outWords(int op, int ops[], int ops_size, char *args[]){ // 处理是否输出限定单词数
+	for(int i = 0; i < ops_size; i++){
+		if(ops[i]){
+			if(op > ops[i] + 2 && !strcmp(args[++ops[i]], "-n"))
+				return atoi(args[++ops[i]]);// 存在,则返回查找到的值
+		}
+	}
+	return que.size(); // 不存在，返回默认所有的单词数
 }
 
 int main(int op, char *args[]){
-	if(op == 3 && !strcmp(args[1], "-f")) solve_f(args);
-	else if(op == 3 && !strcmp(args[1], "-d")) solve_d(args);
-	else if(op == 4 && !strcmp(args[1], "-d") && !strcmp(args[2], "-s")) solve_d_s(args);
-	else cout << "输入格式有误， 命令行参数格式：xxx.exe -f <file name>" << endl;
+	freopen("step_one_out_NoRepetition.txt", "w", stdout); // 结果打印到文件
+	int ops[10]; // 操作数目，记录每一个操作进行到的位置
+	int res = 0;// 操作数下标
+	memset(ops, 0, sizeof ops);
+	if(op >= 3 && !strcmp(args[++ops[res]], "-f")) solve_f(args, ++ops[res]);// -f处理
+	else if(++res && op >=4 && !strcmp(args[++ops[res]], "-d") && !strcmp(args[++ops[res]], "-s"))//-d-s处理
+		solve_d_s(args, ++ops[res]);
+	else if(++res && op >=3 && !strcmp(args[++ops[res]], "-d"))//-d处理
+		solve_d(args, ++ops[res]);
+	else{
+		cout << "输入格式有误， 命令行参数格式：xxx.exe -f <file name>" << endl;
+		return -1;
+	}
+	wordF();
+	print_F(outWords(op, ops, res + 1, args));
 	return 0;
 }
